68a2c2049304702377fe13461914efdde6b932bf
vidhyasree-m <mvidhyasree12177@gmail.com>
2025-02-11 12:23:57 +0530

    commit Day9

---
+++ WebService1/src/main/java/com/example/demo/WebService1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebService1Application.class, args);
+	}
+
+}
