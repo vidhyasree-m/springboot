e6f6679218d7a44062d32c3d9afeabc9f596059f
vidhyasree-m <mvidhyasree12177@gmail.com>
2025-02-11 14:12:12 +0530

    commit Day9

--- SBA/src/main/java/com/example/demo/SBAApplication.java
+++ SBA/src/main/java/com/example/demo/SBAApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebService1Application {
+public class SBAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebService1Application.class, args);
+		SpringApplication.run(SBAApplication.class, args);
 	}
 
 }
